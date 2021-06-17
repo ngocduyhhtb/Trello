0761ecdfa219e1799c52c4ca9249e33a478bdc13
Ngoc Duy <duy191203607@st.utc.edu.vn>
2021-06-17 17:26:14 +0700

    desgin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,150 @@
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="app-container">
+      <div className="navbar app">NavBar</div>
+      <div className="navbar board">NavBar</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/1063/536/354.jpg?hmac=c-ouc6pAVYGm1E0uLcnK_cavuLd8J0VPbodIe_CG8s0"
+                alt=""
+              />
+              Title: My Image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
