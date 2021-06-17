72e7bef2bd0d1d20adcd04a7b6e86df3acd4253e
Ngoc Duy <duy191203607@st.utc.edu.vn>
2021-06-17 17:14:05 +0700

    Degisn Trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,137 @@
-import './App.css';
-
+import "./App.scss";
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="app-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorms</header>
+          <ul>
+            <li>
+              <img
+                src="https://i.picsum.photos/id/599/536/354.jpg?hmac=sPewmcRoY4m3BZK-3XW3G_ACfd2SCfRx6-7DwjQOYek"
+                alt=""
+              />
+              Title: My image
+            </li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+            <li>Add what you'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
